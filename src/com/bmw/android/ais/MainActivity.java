853e82791ec3b13759e8bef07fa1e67d6421922d
varfor <bmw.bookworm@gmail.com>
2014-02-22 21:04:39 -0500

    no change

--- src/com/bmw/android/ais/MainActivity.java
+++ src/com/bmw/android/ais/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends Activity {
 		}
 		return false;
 	}
-
+		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
