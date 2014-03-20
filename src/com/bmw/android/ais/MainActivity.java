cc0a8ea23f270113c15168ca6a22b6231335747a
Benjamin Winger <winger.benjamin@gmail.com>
2014-03-19 20:57:31 -0400

    Removed tests from MainActivity

--- src/com/bmw/android/ais/MainActivity.java
+++ src/com/bmw/android/ais/MainActivity.java
@@ -21,76 +21,13 @@ package com.bmw.android.ais;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.LinearLayout;
-
-import com.bmw.android.androidindexer.FileIndexer;
-
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
-
-import java.io.File;
-import java.io.IOException;
 
 public class MainActivity extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
-		this.setContentView(new LinearLayout(this));
-		IndexReader indexReader;
-		IndexSearcher indexSearcher = null;
-		try {
-			File indexDirFile = new File(FileIndexer.getRootStorageDir());
-			Directory dir = FSDirectory.open(indexDirFile);
-			indexReader = DirectoryReader.open(dir);
-			indexSearcher = new IndexSearcher(indexReader);
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		if (indexSearcher != null) {
-
-			// I don't remember what this line was supposed to do
-			((AISApplication) this.getApplication()).indexSearcher = indexSearcher;
-
-			String field = "text";
-			String value = "Benjamin";
-			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_47);
-			QueryParser parser = new QueryParser(Version.LUCENE_47, field, analyzer);
-
-			try {
-				Log.e("Main", "Searching...");
-				Query query = null;
-				try {
-					query = parser.parse(value);
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				ScoreDoc[] hits = indexSearcher.search(query, 10).scoreDocs;
-				Log.e("Main", "Found " + hits.length + " results");
-				for (ScoreDoc hit : hits) {
-					Document doc = indexSearcher.doc(hit.doc);
-					Log.e("Main", "Found term at: " + doc.getField("id").stringValue() + " " + doc.getField("page").numericValue().longValue());
-				}
-
-			} catch (IOException e) {
-				Log.e("Main", "Error ", e);
-				Log.e("Main", "Failed to search");
-			}
-		}
 	}
 
 	@Override
